b89972a5828853bfa2310f3eb023aa7587b7f810
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-08-13 22:22:24 +0200

    store component

---
+++ src/app/model/model.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductRepository } from './product.repository';
+import { StaticDataSource } from './static.datasource';
+
+@NgModule({
+    declarations: [],
+    imports: [ CommonModule ],
+    exports: [],
+    providers: [ProductRepository, StaticDataSource],
+})
+export class ModelModule { }
