d0275c93ca3ab63eab4f152779198472e249de45
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-08-14 11:25:22 +0200

    new branch, custom directive not working

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,11 +4,12 @@ import { ModelModule } from '../model/model.module';
 import { BrowserModule } from '../../../node_modules/@angular/platform-browser';
 import { StoreComponent } from './store.component';
 import { FormsModule } from '@angular/forms';
+import { CounterDirective } from './counter.directive';
 
 @NgModule({
     declarations: [StoreComponent],
     imports: [ CommonModule, ModelModule, BrowserModule, FormsModule  ],
-    exports: [StoreComponent],
+    exports: [StoreComponent, CounterDirective],
     providers: [],
 })
 export class StoreModule { }
