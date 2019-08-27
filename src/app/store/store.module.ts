e92decb54703a13b767b3257930565c6923f18e7
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-08-27 16:40:56 +0200

    router work not finished

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModelModule } from '../model/model.module';
-import { BrowserModule } from '../../../node_modules/@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { StoreComponent } from './store.component';
 import { FormsModule } from '@angular/forms';
 import { CounterDirective } from './counter.directive';
 import { CartSummaryComponent } from './cartSummary.component';
+import { CartDetailComponent } from './cartDetail.component';
+import { CheckoutComponent } from './checkout.component';
+import { RouterModule } from '../../../node_modules/@angular/router';
 
 @NgModule({
-    declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
-    imports: [ CommonModule, ModelModule, BrowserModule, FormsModule  ],
-    exports: [StoreComponent ],
+    declarations: [StoreComponent,
+        CounterDirective,
+        CartSummaryComponent,
+        CartDetailComponent,
+        CheckoutComponent
+    ],
+    imports: [ CommonModule,
+        ModelModule,
+        BrowserModule,
+        FormsModule,
+        RouterModule
+    ],
+    exports: [StoreComponent, CartDetailComponent, CheckoutComponent ],
     providers: [],
 })
 export class StoreModule { }
