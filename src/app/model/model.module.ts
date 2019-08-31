c3c73a835d591f8ca1c4a479ce3d685e10a409db
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-08-31 14:40:55 +0200

    checkout form

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ProductRepository } from './product.repository';
 import { StaticDataSource } from './static.datasource';
 import { Cart } from './cart.model';
+import { Order } from './order.model';
+import { OrderRepository } from './order.repository';
 
 @NgModule({
     declarations: [],
     imports: [ CommonModule ],
     exports: [],
-    providers: [ProductRepository, StaticDataSource, Cart],
+    providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository],
 })
 export class ModelModule { }
