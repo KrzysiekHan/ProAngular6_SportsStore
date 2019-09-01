e67db9ecc65fe169e6ca23917b36af32ee8998fd
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-09-01 22:01:15 +0200

    model.module.ts static data source substitution

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -5,11 +5,14 @@ import { StaticDataSource } from './static.datasource';
 import { Cart } from './cart.model';
 import { Order } from './order.model';
 import { OrderRepository } from './order.repository';
+import { RestDataSource } from './rest.datasource';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [],
-    imports: [ CommonModule ],
+    imports: [ CommonModule, HttpClientModule ],
     exports: [],
-    providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository],
+    providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository, 
+    { provide: StaticDataSource, useClass: RestDataSource}],
 })
 export class ModelModule { }
