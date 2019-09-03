d7e701ad44d30249e4cfd50dd9e60c444505b289
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-09-03 23:16:18 +0200

    authorization guard

--- src/app/admin/auth.component.ts
+++ src/app/admin/auth.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '../../../node_modules/@angular/router';
 import { NgForm } from '../../../node_modules/@angular/forms';
+import { AuthService } from '../model/auth.service';
 
 @Component({
     templateUrl: 'auth.component.html',
@@ -10,13 +11,18 @@ export class AuthComponent {
     public username: string;
     public password: string;
     public errorMessage: string;
-    constructor(private router: Router) { }
+    constructor(private router: Router, private auth: AuthService) { }
 
     authenticate(form: NgForm) {
         if (form.valid) {
-            this.router.navigateByUrl("/admin/main");
+            this.auth.authenticate(this.username, this.password)
+            .subscribe(response => {
+                if  (response) {
+                    this.router.navigateByUrl('/admin/main');
+                }
+            });
         } else {
-            this.errorMessage = "Form Data Invalid";
+            this.errorMessage = 'Form Data Invalid';
         }
     }
 }
