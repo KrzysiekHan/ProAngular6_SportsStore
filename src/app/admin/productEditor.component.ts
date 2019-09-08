8a8cc1a33122156051f556e8fd7318ce2f2c601e
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-09-08 15:44:38 +0200

    debug edit product component

--- src/app/admin/productEditor.component.ts
+++ src/app/admin/productEditor.component.ts
@@ -1,26 +1,30 @@
 import { Component } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
-import { ActivatedRoute } from '../../../node_modules/@angular/router';
+import { ActivatedRoute, Router } from '../../../node_modules/@angular/router';
 import { NgForm } from '@angular/forms';
 
 @Component({
     templateUrl: 'productEditor.component.html'
 })
+
 export class ProductEditorComponent {
     editing: boolean = false;
     product: Product = new Product();
+    id: number;
 
     constructor(private repository: ProductRepository,
         private router: Router,
         activeRoute: ActivatedRoute) {
 
-            this.editing = activeRoute.snapshot.params["mode"] == "edit";
+            this.editing = activeRoute.snapshot.params['mode'] == "edit";
+            this.id = activeRoute.snapshot.params['id'];
             if (this.editing) {
-                Object.assign(this.product,
-                    repository.getProduct(activeRoute.snapshot.params["id"]));
+                Object.assign(this.product, repository.getProduct(this.id));
+                console.log(this.product);
             }
         }
+
     save(form: NgForm) {
         this.repository.saveProduct(this.product);
         this.router.navigateByUrl("/admin/main/products")
--- src/app/model/product.repository.ts
+++ src/app/model/product.repository.ts
@@ -19,7 +19,7 @@ export class ProductRepository {
     }
 
     getProduct(id: number): Product {
-        return this.products.find(p => p.id === id);
+        return this.products.find(p => p.id == +id);
     }
 
     getCategories(): string[] {
@@ -27,14 +27,14 @@ export class ProductRepository {
     }
 
     saveProduct(product: Product) {
-        if (product.id == null || product.id == 0) {
+        if (product.id == null || product.id === 0) {
             this.dataSource.saveProduct(product)
             .subscribe(p => this.products.push(p));
         } else {
             this.dataSource.updateProduct(product)
             .subscribe(p => {
                 this.products.splice(this.products
-                .findIndex(p => p.id == product.id), 1, product);
+                .findIndex(p => p.id === product.id), 1, product);
             });
         }
     }
@@ -42,8 +42,8 @@ export class ProductRepository {
     deleteProduct(id: number) {
         this.dataSource.deleteProduct(id).subscribe(p => {
             this.products.splice(this.products.
-            findIndex(p => p.id == id), 1);
-        })
+            findIndex(p => p.id === id), 1);
+        });
     }
 
 }
